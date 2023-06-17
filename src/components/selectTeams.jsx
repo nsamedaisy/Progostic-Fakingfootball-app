7873ba20a6444fb95e7df4cba4f1ab2dad7812a3
nsamedaisy <nsamedaisy@gmail.com>
2023-06-17 17:20:16 +0100

    fakefootball commit

--- src/components/selectTeams.jsx
+++ src/components/selectTeams.jsx
@@ -18,17 +18,17 @@ export const SelectTeams = () => {
     <div className="board">
       <div>
         <h1 className="heading-1">
-          Progostic Football <span>LiveScore</span> match
+          Progostic <span> Football</span> LiveScore Match
         </h1>
 
         <div className="row">
           <div>
-            <label className="matchtype">Clubs</label>
+            <label>Clubs</label>
             <div className="select">
               {clubTeam?.map((club, i) => {
                 return (
-                  <div key={club.name + i}>
-                    <img src={club.url} alt="logo" />
+                  <div key={club.name + i} className="flex-list">
+                    <img src={club.url} alt="logo" className="flags" />
                     <h3>{club.name}</h3>
                   </div>
                 );
@@ -37,12 +37,12 @@ export const SelectTeams = () => {
           </div>
 
           <div>
-            <label className="matchtype">Countries</label>
+            <label>Countries</label>
             <div className="select">
               {countryTeam?.map((country, i) => {
                 return (
-                  <div key={country.name + i}>
-                    <img src={country.flag} alt="flags" />
+                  <div key={country.name + i} className="flex-list">
+                    <img src={country.flag} alt="flags" className="flags" />
                     <h3>{country.country}</h3>
                   </div>
                 );
--- src/components/teamScore.jsx
+++ src/components/teamScore.jsx
@@ -4,19 +4,26 @@ export const TeamScore = () => {
   return (
     <div className="choosen-team">
       <div>
-        <h1>Home</h1>
+        <h1 className="head1">
+          <span>H</span>ome
+        </h1>
         <img src="" alt="flag" className="league-logo" />
-        <h2 className="league-name">kammerrooofdksnvnf</h2>
+        <h2 className="league-name">Canada</h2>
       </div>
 
       <div>
-        <p className="results"></p>
+        <p className="results">
+          {" "}
+          3 <span>vs</span> 2{" "}
+        </p>
       </div>
 
       <div>
-        <h1>Away</h1>
+        <h1 className="head1">
+          <span>A</span>way
+        </h1>
         <img src="" alt="flag" className="league-logo" />
-        <h2 className="league-name">camerron</h2>
+        <h2 className="league-name">Cameroon</h2>
       </div>
     </div>
   );
